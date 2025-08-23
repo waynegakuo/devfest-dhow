7b6cfb0afb752b01a8b64bd025ec74ec3de8d933
Wayne <waynejotham@gmail.com>
2025-08-23 16:17:18 +0300

    feat(configuration): integrate Firebase configuration and services into app environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment.development';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(()=> getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyB5241D60oJ9PfuGVJVLgs0Hb7Ytdr_B1k",
+    authDomain: "devfest-dhow.firebaseapp.com",
+    projectId: "devfest-dhow",
+    storageBucket: "devfest-dhow.firebasestorage.app",
+    messagingSenderId: "279874943258",
+    appId: "1:279874943258:web:2e4ae0838694752916d4a3",
+    measurementId: "G-WXCS3PG7S6"
+  }
+};
