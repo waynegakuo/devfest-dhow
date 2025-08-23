e21700cff4e346f0a02a55ddc777158ab337ef3e
Wayne <waynejotham@gmail.com>
2025-08-23 19:34:36 +0300

    chore: remove CompassNavComponent and associated styles, templates, and tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CompassNavComponent } from './shared/components/compass-nav/compass-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CompassNavComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
