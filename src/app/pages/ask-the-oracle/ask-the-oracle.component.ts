3f1f74e14df474ac0f52a2fd9f699ec1f09f04ad
Wayne Gakuo <waynejotham@gmail.com>
2025-08-31 00:47:41 +0300

    feat(dashboard): Add new pages and components for improved user experience
    - Introduce "Codelab Doubloons" and "Ask the Oracle" standalone components with tailored layouts and styles.
    - Add "Archipelago" page showcasing the full schedule and voyage tracks with session details.
    - Implement "My Voyage Plan" feature for managing planned sessions with clear functionality and styling.
    - Develop reusable "Dashboard Layout" component with sidebar state management and responsive design.
    - Enhance overall SCSS structure for consistency and better maintainability.

---
+++ src/app/pages/ask-the-oracle/ask-the-oracle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ask-the-oracle',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ask-the-oracle.component.html',
+  styleUrl: './ask-the-oracle.component.scss'
+})
+export class AskTheOracleComponent {
+}
---
+++ src/app/pages/navigational-drills/navigational-drills.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navigational-drills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navigational-drills.component.html',
+  styleUrl: './navigational-drills.component.scss'
+})
+export class NavigationalDrillsComponent {
+}
