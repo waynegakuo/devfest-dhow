ab42fb3540e9255f1d63d5afca216184ca8a862c
Wayne Gakuo <waynejotham@gmail.com>
2025-11-27 09:56:10 +0300

    feat(oracle): add floating button as a shortcut to the oracle page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { OracleFabComponent } from './components/oracle-fab/oracle-fab.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, OracleFabComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
